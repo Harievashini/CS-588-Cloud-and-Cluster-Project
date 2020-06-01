289cd9ecc2203178c082c1bd3512c2809f07e60c
Harievashini <50682874+Harievashini@users.noreply.github.com>
2020-05-31 21:22:33 -0700

    Update query2.py

--- query2.py
+++ query2.py
@@ -1,23 +1,25 @@
+# Importing pymongo package and datetime package
 from pymongo import MongoClient
 from datetime import datetime
 
+# Passing MongoClient a host name and a port number.
 client = MongoClient("mongodb://127.0.0.1:27017")
-detector=[]
-volume_count=[]
-sum_volume=0
+detector=[] #List to store the detector ids
+volume_count=[] # List to hold the volume counts
+sum_volume=0 # Total volume
 with client:
-	db = client.project
-	data = db.metadata_station.find({'locationtext':'Foster NB'})
+	db = client.project # Opening Database project
+	data = db.metadata_station.find({'locationtext':'Foster NB'}) # Retrieving the documents whose locationtext is Foster NB
 	for station in data:
-		r=station['detectors']
+		r=station['detectors'] # To fetch the detectors object
 		for i in r:
 			detector.append(i['detectorid'])
 	for i in detector:
-		#data1=db.loopdata.find({'detectorid':i,"starttime" : {"$gte":datetime.fromisoformat('2011-09-21T00:00:00.070+00:00'),"$lt":datetime.fromisoformat('2011-09-22T00:00:00.070+00:00')}})
 		aggregate_data = [{ '$match': {'$and': [ { 'detectorid':i }, { "starttime" : {"$gte":datetime.fromisoformat('2011-09-21T00:00:00.070+00:00'),"$lt":datetime.fromisoformat('2011-09-22T00:00:00.070+00:00')}}] }},{ '$group': {'_id': "null", 'sumofvolume': { '$sum': "$volume" } }}]
-		vol = list(db.loopdata.aggregate(aggregate_data))
+		vol = list(db.loopdata.aggregate(aggregate_data)) #volume count of the documents from loopdata collection
 		volume_count.append(vol)
 	for i in volume_count:
-		sum_volume +=i[0]['sumofvolume']	
-	print("Total volume = ",sum_volume)
+		sum_volume +=i[0]['sumofvolume'] #Total volume
+	print("Total volume :",sum_volume)
+		
 		
