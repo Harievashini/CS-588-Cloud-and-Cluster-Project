cce80840b6cf84edfb65e616ed06b51e48c46080
Harievashini <50682874+Harievashini@users.noreply.github.com>
2020-05-31 21:21:01 -0700

    Update query1.py

--- query1.py
+++ query1.py
@@ -1,7 +1,9 @@
+# Importing pymongo package
 from pymongo import MongoClient
 
+# Passing MongoClient a host name and a port number.
 client = MongoClient("mongodb://127.0.0.1:27017")
-db = client.project
-data = db.loopdata.count_documents({'speed':{"$gt":100}})
-print(data)
+db = client.project # Opening Database project
+data = db.loopdata.count_documents({'speed':{"$gt":100}}) # Counting the number of records that have high speed from loopdata collection
+print("Count high speeds :",data)
 	
